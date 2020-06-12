ed05d57d1b435e116aded54816afd5ad86d82546
Stanley Sakai <stanley@stanographer.com>
2020-06-12 12:50:41 -0400

    rounded

--- src/components/Shared/team-box.js
+++ src/components/Shared/team-box.js
@@ -40,7 +40,7 @@ class TeamBox extends Component {
                       alt: candidate.name,
                       height: "100%",
                       className:
-                        "img-fluid avatar avatar-ex-large z-depth-1-half shadow",
+                        "img-fluid avatar avatar-ex-large rounded-pill z-depth-1-half shadow",
                     }}
                   />
                 </div>
